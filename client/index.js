da8aa89f3353539ac670dbc36718d2f3626da92c
gonzalober <gonzaloberhouet@gmail.com>
2021-07-21 11:08:36 +0100

    moce script html to js

---
+++ client/index.js
@@ -0,0 +1,32 @@
+const tweetStream = document.getElementById("tweetStream");
+const socket = io();
+
+const tweets = [];
+
+socket.on("connect", () => {
+  console.log("Connected to server...");
+});
+
+socket.on("tweet", (tweet) => {
+  // console.log(tweet)
+  const tweetData = {
+    id: tweet.data.id,
+    text: tweet.data.text,
+    username: `@${tweet.includes.users[0].username}`,
+  };
+
+  const tweetEl = document.createElement("div");
+  tweetEl.className = "card my-4";
+  tweetEl.innerHTML = `
+            <div class="card-body">
+                <h5 class="card-title">${tweetData.text}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${tweetData.username}</h6>
+                
+                <a class="btn btn-primary mt-3" href="https://twitter.com/${tweetData.username}/status/${tweetData.id}">
+                    <i class="fab fa-twitter"></i> Go To Tweet    
+                </a>
+            </div>
+        `;
+
+  tweetStream.appendChild(tweetEl);
+});
--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const needle = require("needle");
 const config = require("dotenv").config();
 const TOKEN = process.env.TWITTER_TOKEN;
 
+app.use(express.static("client"));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../", "client", "index.html"));
 });
@@ -18,7 +19,7 @@ const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
 const streamURL =
   "https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id";
 
-const rules = [{ value: "Brexit" }];
+const rules = [{ value: "Argentina" }];
 
 //get stream rules
 const getRules = async () => {
